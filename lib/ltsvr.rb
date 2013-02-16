fdfe05e848536f16596fb3ab9d91484c0ad0101c
ongaeshi <ongaeshi0621@gmail.com>
2013-02-16 16:20:49 +0900

    Separate file

--- lib/ltsvr.rb
+++ lib/ltsvr.rb
@@ -1,4 +1,5 @@
 require "ltsvr/version"
+require "ltsvr/cli"
 
 module Ltsvr
   # Your code goes here...
---
+++ lib/ltsvr/cli.rb
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# @file 
+# @brief
+# @author ongaeshi
+# @date   2013/02/16
+
+require 'rubygems'
+require 'ltsv'
+require 'optparse'
+
+module Ltsvr
+  class CLI
+    def self.execute(stdout, arguments=[])
+      options = {}
+      options[:keywords]        = []
+      options[:ignore_keywords] = []
+      options[:filters]         = []
+      
+      opts = OptionParser.new("#{File.basename($0)}")
+      opts.on("-k", "--keywords"       , "Display keywords   (-k host,ua)")                      {|v| options[:keywords]        << v }
+      opts.on("-i", "--ignore-keywords", "Ignore keywords    (-i time,req)")                     {|v| options[:ignore_keywords] << v }
+      opts.on("-f", "--filter"         , "Filtering keywords (-f host=192.168.1.1,ua=~Mozilla)") {|v| options[:filters]         << v }
+      opts.on("--web"                  , "Go to website(http://ltsv.org)")                       {|v| options[:web] = true }
+      opts.parse!(arguments)
+
+      while line = gets
+        p LTSV.parse(line)[0]
+      end
+    end
+  end
+end
+
+
