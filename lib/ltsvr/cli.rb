71bb73c72b964d7de7510dd2ba1c6cd6422f633b
ongaeshi <ongaeshi0621@gmail.com>
2013-02-16 18:25:56 +0900

    Add option args

--- lib/ltsvr/cli.rb
+++ lib/ltsvr/cli.rb
@@ -18,10 +18,10 @@ module Ltsvr
       options[:filters]         = []
       
       opts = OptionParser.new("#{File.basename($0)}")
-      opts.on("-k", "--keywords"       , "Display keywords   (-k host,ua)")                      {|v| options[:keywords]        << v }
-      opts.on("-i", "--ignore-keywords", "Ignore keywords    (-i time,req)")                     {|v| options[:ignore_keywords] << v }
-      opts.on("-f", "--filter"         , "Filtering keywords (-f host=192.168.1.1,ua=~Mozilla)") {|v| options[:filters]         << v }
-      opts.on("--web"                  , "Go to website(http://ltsv.org)")                       {|v| options[:web] = true }
+      opts.on("-k LABEL", "--keywords"       , "Display keywords   (-k host,ua)")                      {|v| options[:keywords]        << v }
+      opts.on("-i LABEL", "--ignore-keywords", "Ignore keywords    (-i time,req)")                     {|v| options[:ignore_keywords] << v }
+      opts.on("-f FILTER", "--filter"        , "Filtering keywords (-f host=192.168.1.1,ua=~Mozilla)") {|v| options[:filters]         << v }
+      opts.on("--web"                        , "Go to website (http://ltsv.org)")                      {|v| options[:web] = true }
       opts.parse!(arguments)
 
       arguments.each do |filename|
